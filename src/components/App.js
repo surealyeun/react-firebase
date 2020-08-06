d250595eb51bc4522fcb9c4172a8e83b5529fdd6
한수연 <monkyw@naver.com>
2020-08-06 19:41:04 +0900

    [20200806] react firebase 연동해서 앱 배포하기 시작

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  reder() {
+    return (
+      <div>
+        <h3>Hello!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
